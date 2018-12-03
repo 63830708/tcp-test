2690543f8a29a03d4cc2e06cb5add96655ba31e1
chenwenjie <chenwenjie@kongking.net>
2018-12-03 14:12:50 +0800

    v 1.04 将服务端改成select

--- MyServer/server.cpp
+++ MyServer/server.cpp
@@ -1,6 +1,53 @@
 ﻿#include <QCoreApplication>
+#include <vector>
+#include <algorithm>
+#include <functional>
 #include "util.h"
 
+
+std::vector<SOCKET> g_clients;
+
+int processor(SOCKET sockClient)
+{
+    // 接收消息并反馈
+    DataHeader header = {};
+    int nLen = recv(sockClient, (char*)&header, sizeof(DataHeader), 0);
+    if(nLen <= 0)
+    {
+        LOG << "recv()<=0 \n";
+        return -1;
+    }
+
+    LOG << "recv: cmd-" << header.cmd << "len-" << header.len << endl;
+    switch(header.cmd)
+    {
+    case CMD_LOGIN:
+    {
+        Login login{};
+        recv(sockClient, (char*)&login+sizeof(DataHeader), sizeof(Login)-sizeof(DataHeader), 0);
+        LOG << "recv: login    username-" << login.userName << "pwd-" << login.password << endl;
+        LoginResult ret{1};
+        send(sockClient, (char*)&ret, sizeof(LoginResult), 0);
+    }
+        break;
+    case CMD_LOGOUT:
+    {
+        Logout logout{};
+        recv(sockClient, (char*)&logout+sizeof(DataHeader), sizeof(Logout)-sizeof(DataHeader), 0);
+        LOG << "recv: logout    username-" << logout.useName << endl;
+        LogoutResult ret{1};
+        send(sockClient, (char*)&ret, sizeof(LogoutResult), 0);
+    }
+        break;
+    default:
+        header.cmd = CMD_ERROR;
+        header.len = 0;
+        send(sockClient, (char*)&header, sizeof(DataHeader), 0);
+        break;
+    }
+
+    return 0;
+}
 
 int main(int argc, char *argv[])
 {
@@ -19,7 +66,7 @@ int main(int argc, char *argv[])
     addrServer.sin_addr.S_un.S_addr = inet_addr(SERVER_IP); // INADDR_ANY
     if(SOCKET_ERROR == bind(sockServer, (sockaddr*)&addrServer, sizeof(sockaddr_in)))
     {
-        LOG << "bind() failed." << errno;
+        LOG << "bind() failed." << errno << endl;
         return -1;
     }
     else
@@ -38,64 +85,80 @@ int main(int argc, char *argv[])
         LOG << "listen() succeed.\n";
     }
     // 等待并接受客户端连接
-    sockaddr_in addrClient{};
-    int nAddrLen = sizeof(sockaddr_in);
-    SOCKET sockClient = INVALID_SOCKET;
-    sockClient = accept(sockServer, (sockaddr*)&addrClient, &nAddrLen);
-    if(INVALID_SOCKET == sockClient)
-    {
-        LOG << "accept() failed.\n";
-        return -1;
-    }
-    else
-    {
-        LOG << "client accepted: socket-" << hex<< (int)sockClient
-            << "\tip-" << dec << inet_ntoa(addrClient.sin_addr)
-            << ":" << addrClient.sin_port;
-    }
-
-    // 接收消息并反馈
+    fd_set fdRead;
+    fd_set fdWrite;
+    fd_set fdExp;
     while(true)
     {
-        DataHeader header = {};
-        int nLen = recv(sockClient, (char*)&header, sizeof(DataHeader), 0);
-        if(nLen <= 0)
+        FD_ZERO(&fdRead);
+        FD_ZERO(&fdWrite);
+        FD_ZERO(&fdExp);
+
+        FD_SET(sockServer, &fdRead);
+        FD_SET(sockServer, &fdWrite);
+        FD_SET(sockServer, &fdExp);
+
+        for(int i=g_clients.size()-1; i>=0; i--)
         {
-            LOG << "recv()<=0 \n";
+            FD_SET(g_clients[i], &fdRead);
+        }
+
+        timeval t{0, 0};
+        //        LOG << "start select.";
+        int ret = select(sockServer+1, &fdRead, &fdWrite, &fdExp, 0);//&t);
+        //        LOG << "end   select.";
+        if(ret < 0)
+        {
+            LOG << "select() failed.";
             break;
         }
 
-        LOG << "recv: cmd-" << header.cmd << "len-" << header.len;
-        switch(header.cmd)
+        // 是否有客户端接入
+        if( FD_ISSET(sockServer, &fdRead))
         {
-        case CMD_LOGIN:
-        {
-            Login login{};
-            recv(sockClient, (char*)&login+sizeof(DataHeader), sizeof(Login)-sizeof(DataHeader), 0);
-                LOG << "recv: login    username-" << login.userName << "pwd-" << login.password;
-            LoginResult ret{1};
-            send(sockClient, (char*)&ret, sizeof(LoginResult), 0);
+            FD_CLR(sockServer, &fdRead);
+            sockaddr_in addrClient{};
+            int nAddrClientLen = sizeof(sockaddr_in);
+            SOCKET sockClient = accept(sockServer, (sockaddr*)&addrClient, &nAddrClientLen);
+            if(INVALID_SOCKET == sockServer)
+            {
+                LOG << "accept() failed.\n";
+            }
+            else
+            {
+                g_clients.push_back(sockClient);
+                LOG << "client accepted: socket-" << hex<< (int)sockClient
+                    << "\tip-" << dec << inet_ntoa(addrClient.sin_addr)
+                    << ":" << addrClient.sin_port<< endl;
+            }
+            --ret;
         }
-            break;
-        case CMD_LOGOUT:
-        {
-            Logout logout{};
-            recv(sockClient, (char*)&logout+sizeof(DataHeader), sizeof(Logout)-sizeof(DataHeader), 0);
-                LOG << "recv: logout    username-" << logout.useName;
-            LogoutResult ret{1};
-            send(sockClient, (char*)&ret, sizeof(LogoutResult), 0);
-        }
-            break;
-        default:
-            header.cmd = CMD_ERROR;
-            header.len = 0;
-            send(sockClient, (char*)&header, sizeof(DataHeader), 0);
-            break;
-        }
+        // 处理可读的socket
+        for(decltype(fdRead.fd_count) i=0; i<fdRead.fd_count ;i++)
 
+        {
+            if(-1 == processor(fdRead.fd_array[i]))
+            {
+                //auto iter = std::find_if(g_clients.begin(), g_clients.end(), std::bind1st(FD_ISSET(),fdRead.fd_array[i]));
+                auto iter=std::find(g_clients.begin(), g_clients.end(), fdRead.fd_array[i]);
+                if(g_clients.end() != iter)
+                {
+                    closesocket(*iter);
+                    g_clients.erase(iter);
+
+                }
+            }
+        }
     }
 
-    closesocket(sockClient);
+    for( auto s: g_clients)
+    {
+        closesocket(s);
+    }
+
+
+
+    closesocket(sockServer);
 
     WSACleanup();
 
@@ -103,8 +166,6 @@ int main(int argc, char *argv[])
 
 
 
-
-
     return 0;
-//    return a.exec();
+    //    return a.exec();
 }
